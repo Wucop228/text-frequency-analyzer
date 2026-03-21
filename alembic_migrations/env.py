6e786610820a1c2f431ca49ccd2c32c882880dc2
Roma <you@example.com>
2026-03-22 01:27:32 +0300

    add Report model to .env and apply migrate to create report table

--- alembic_migrations/env.py
+++ alembic_migrations/env.py
@@ -21,6 +21,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from app.core.database import Base
+from app.report.models import Report
 
 target_metadata = Base.metadata
 
